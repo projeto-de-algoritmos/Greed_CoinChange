5a141dc2397cf7b7e0f92ad8482cdd957e5527c0
Paulo Victor de Menezes Lopes <pv.paulovictor.vp@gmail.com>
2019-10-14 08:19:57 -0300

    Alterando entradas do programa

--- Greed_CoinChange.c
+++ Greed_CoinChange.c
@@ -7,18 +7,15 @@
 #define NOTAS {100, 50, 20, 10, 5, 2}
 #define MOEDAS {100, 50, 25, 10, 5, 1}
 
-int main() {
-
-    int notas[MAX_NOTAS]= NOTAS;
-    int moedas[MAX_MOEDAS] = MOEDAS;
-    int inteiro = 0, decimal = 0, aux;
-
-    printf("\tSistema de troco:\n\n");
+void menu(int notas[], int moedas[]) {
+    printf("\n\tSistema de troco:\n\n");
     printf("Para o troco, temos as seguintes notas e moedas:\n");
+
     printf("Notas de ");
     for (int i = 0; i < MAX_NOTAS; i++) {
         printf("%d, ", notas[i]);
     }
+
     printf("e moedas de ");
     for (int i = 0; i < MAX_MOEDAS; i++) {
         if(i == 0) {
@@ -29,12 +26,11 @@ int main() {
             printf("e 1 centavo.\n");
         }
     }
+}
 
-    printf("\n");
-    printf("Insira o valor do troco: Ex:(00,00)\n");
-    scanf("%d,%d", &inteiro, &decimal);
+void troco(int notas[], int moedas[], int inteiro, int decimal) {
+    int aux;
 
-    printf("\n");
     printf("Foram utilizadas as seguintes notas e moedas para o troco:\n");
     for (int i = 0; i < MAX_NOTAS; i++) {
         aux = inteiro/notas[i];
@@ -58,11 +54,44 @@ int main() {
         if (aux) {
             if (i == 0) {
                 printf("%d moedas de 1 REAL\n", aux);
-            }
-            else {
+            } else {
                 printf("%d moedas de %d centavos\n", aux, moedas[i]);
             }
         }
     }
-return 0;
+    printf("\n");
+    return;
+}
+
+int main() {
+
+    int notas[MAX_NOTAS] = NOTAS;
+    int moedas[MAX_MOEDAS] = MOEDAS;
+    int inteiro = 0, decimal = 0;
+    float valor_compra = 0, valor_pago = 0,valor_troco = 0;
+
+    do {
+        menu(notas, moedas);
+
+        printf("Insira o valor da compra: Ex:(00.00)\n");
+        scanf("%f", &valor_compra);
+        if (valor_compra) {
+            printf("Insira o valor pago: Ex:(00.00)\n");
+            scanf("%f", &valor_pago);
+            
+            valor_troco = valor_pago - valor_compra;
+            inteiro = valor_troco/1;
+            decimal = 100 * (valor_troco - inteiro);
+
+            printf("Troco: %d,%d\n",inteiro,decimal);
+
+            printf("\n");
+            if (inteiro != 0 || decimal != 0) {
+                troco(notas, moedas, inteiro, decimal);
+            }
+        }
+        
+    } while(valor_compra != 0);
+
+    return 0;
 }
